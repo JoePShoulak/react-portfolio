033d5cbeed136c1de49ee3c2e0cc8726f471649c
Joe P Shoulak <joe@squarecompass.io>
2022-11-17 17:16:39 -0600

    implemented MUI for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
-function Footer() {
+import * as React from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Paper from "@mui/material/Paper";
+import { Typography } from "@mui/material";
+
+export default function FixedBottomNavigation() {
   return (
-    <>
-      <span>Made in React by Joe P Shoulak</span>
-    </>
+    <Box sx={{ pb: 7 }}>
+      <CssBaseline />
+
+      <Paper
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <Typography align="center">Made by Joe P. Shoulak</Typography>
+      </Paper>
+    </Box>
   );
 }
-
-export default Footer;
